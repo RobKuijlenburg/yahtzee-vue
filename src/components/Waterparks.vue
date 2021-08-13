78c6cd53ed0187516dc2dab8c4419df67e5dcac5
Rob <robkuijlenburg91@gmail.com>
2021-08-13 14:02:04 +0200

    Added checks for straights and kinds

--- src/components/Waterparks.vue
+++ src/components/Waterparks.vue
@@ -6,7 +6,7 @@
         You threw:
       </p>
       <table>
-        <tr v-for="(dice, index) in diceArray" :key="dice-{index}">
+        <tr v-for="(dice, index) in diceArray" :key="index">
           <td>
             {{index}}
           </td>
@@ -14,6 +14,79 @@
             {{dice}}
           </td>
         </tr>
+
+        <tr>
+          <td>
+            Three of a kind
+          </td>
+          <td>
+            {{containsThreeOfAKind}}
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            Four of a kind
+          </td>
+          <td>
+            {{containsFourOfAKind}}
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            Fullhouse
+          </td>
+          <td>
+            {{containsFullHouse}}
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            Small straight
+          </td>
+          <td>
+            {{getSmStraightScore}}
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            Large straight
+          </td>
+          <td>
+            {{getLgStraightScore}}
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            Yahtzee
+          </td>
+          <td>
+            {{containsYahtzee}}
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            Chance
+          </td>
+          <td>
+            {{getChance}}
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            Total
+          </td>
+          <td>
+            {{getTotal}}
+          </td>
+        </tr>
+
       </table>
 
     </div>
@@ -30,20 +103,26 @@ export default {
   data() {
     return {
       diceArray: {},
-    }
+      straightCheck: [],
+      semiTotal: 0,
+      total: 0
+    };
+
   },
   
   methods: {
-    clearDice() {
-      this.diceArray={};
-    },
-
     throwDice() {
           for (let i = 0 ; i < 5 ; i++) {
           var dice = Math.floor(Math.random()*DICE_SIDES) + 1;
           this.diceArray[dice]++;
+          this.semiTotal += dice;
+          this.straightCheck.push(dice);
       }
+      this.straightCheck.sort();
+      this.total = this.semiTotal + this.containsThreeOfAKind + this.containsFullHouse + this.containsFourOfAKind + this.containsYahtzee + this.getSmStraightScore + this.getLgStraightScore;
       console.log(this.diceArray);
+      console.log(this.semiTotal);
+      console.log(this.straightCheck)
     },
 
     resetThrows() {
@@ -51,30 +130,8 @@ export default {
             for (let side = 1; side <= DICE_SIDES; side++) {
                 this.$set(this.diceArray, side, 0)
             }
+            console.log(this.diceArray);
     },
-
-    fullHouse(obj, key){
-        if(Object.keys(obj).includes(key)){
-          console.log('full house');
-        }
-
-    },
-
-    getSmStraightScore() {
-        let joinDices = this.diceArray.join('');
-        let repDices = joinDices.toString().replace(/(.)\1/,'$1');
-        let x = (/1234|2345|3456/.test(repDices));
-        console.log('Small straight');
-        return x ? 30 : 0;
-    },
-
-    getLgStraightScore() {
-        let joinDices = this.diceArray.join('');
-        let repDices = joinDices.toString().replace(/(.)\1/,'$1');
-        let x = (/12345|23456/.test(repDices));
-        console.log('Large Straight');
-        return x ? 40 : 0;
-    }
   },
   
   props: {
@@ -84,8 +141,68 @@ export default {
     containsFullHouse() {
       // return it so it can be processed
       // add logic: Object.values(this.diceArray) ....
-      Object.values(this.diceArray).includes(3 && 2)
-      return console.log('fullhouse');
+      if(Object.values(this.diceArray).includes(3) && Object.values(this.diceArray).includes(2)){
+        return 25;
+      } else {
+        return 0;
+      }
+    },
+
+    containsThreeOfAKind() {
+      if (Object.values(this.diceArray).includes(3)){
+        return this.semiTotal;
+      } else {
+        return 0;
+      }
+    },
+
+    containsFourOfAKind() {
+      if (Object.values(this.diceArray).includes(4)){
+        return this.semiTotal;
+      } else {
+        return 0;
+      }
+    },
+
+    getSmStraightScore() {
+        let joinDices = this.straightCheck.join('');
+        let repDices = joinDices.toString().replace(/(.)\1/,'$1');
+        let x = (/1234|2345|3456/.test(repDices));
+        if (x === true) {
+           console.log('Small straight');
+          return 30;
+        } else {
+          return 0;
+        }
+       
+    },
+
+    getLgStraightScore() {
+        let joinDices = this.straightCheck.join('');
+        let repDices = joinDices.toString().replace(/(.)\1/,'$1');
+        let x = (/12345|23456/.test(repDices));
+        if (x === true) {
+          console.log('Large straight');
+          return 40;
+        } else {
+          return 0;
+        }
+    },
+
+    containsYahtzee() {
+      if (Object.values(this.diceArray).includes(5)){
+        return 50;
+      } else {
+        return 0;
+      }
+    },
+
+    getChance() {
+      return this.semiTotal;
+    },
+
+    getTotal(){
+      return this.total;
     }
   },
 
